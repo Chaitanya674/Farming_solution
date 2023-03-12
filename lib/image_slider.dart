4d498cfb3632320cd58604b1c31281c9d7f880b9
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-12 16:36:46 +0530

    updating files

--- lib/image_slider.dart
+++ lib/image_slider.dart
@@ -2,81 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class ImageSlider extends StatelessWidget {
-  const ImageSlider({ Key? key }) : super(key: key);
+  const ImageSlider({Key? key}) : super(key: key);
+
+  // List of image URLs (you may want to fix the paths)
+  final List<String> imageUrls = [
+    "assets/images/Agriculture-Infrastructure-.jpg",
+    "/assets/images/110322-SBI-Yono-Krishi_Customer_Hindi_Web-banner_1820px-(w)-X-831px-(h)_2.jpg",
+    "/assets/images/bima-yojna.jpg",
+    "assets/images/PM-Matsya-Sampada-Yojana.jpg",
+    "assets/images/Pradhan-Mantri-Kisan-Maan-Dhan-Yojana-in-Hindi.jpg",
+    "assets/images/Pradhan-Mantri-Kisan-Samman-Nidhi.png",
+    "assets/images/Pradhan-Mantri-Krishi-Sinchai-Yojana.png",
+    // Add more image URLs as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: CarouselSlider(
-        items: [
-          // 1st Image of Slider
-          Container(
+        items: imageUrls.map((imageUrl) {
+          return Container(
             margin: EdgeInsets.all(6.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8.0),
               image: DecorationImage(
-                // Load image from a network URL (you may want to fix the path)
-                image: NetworkImage("assets/images/Agriculture-Infrastructure-.jpg"),
+                // Load image from the list of image URLs
+                image: NetworkImage(imageUrl),
                 fit: BoxFit.fill,
               ),
             ),
-          ),
-          
-          // 2nd Image of Slider
-          Container(
-            margin: EdgeInsets.all(6.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                // Load image from a network URL (you may want to fix the path)
-                image: NetworkImage("/assets/images/110322-SBI-Yono-Krishi_Customer_Hindi_Web-banner_1820px-(w)-X-831px-(h)_2.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          
-          // 3rd Image of Slider
-          Container(
-            margin: EdgeInsets.all(6.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                // Load image from a network URL (you may want to fix the path)
-                image: NetworkImage("/assets/images/bima-yojna.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          
-          // 4th Image of Slider
-          Container(
-            margin: EdgeInsets.all(6.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                // Load image from a network URL (you may want to fix the path)
-                image: NetworkImage("assets/images/PM-Matsya-Sampada-Yojana.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          
-          // 5th Image of Slider
-          Container(
-            margin: EdgeInsets.all(6.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                // Load image from a network URL (you may want to fix the path)
-                image: NetworkImage("assets/images/Pradhan-Mantri-Kisan-Maan-Dhan-Yojana-in-Hindi.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          
-          // Additional Images...
-          
-        ],
+          );
+        }).toList(),
         // Slider Container properties
         options: CarouselOptions(
           height: 350.0,
