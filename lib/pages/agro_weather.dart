203c05bae96f6f98abc7f3717b2f5a9455023802
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-21 16:58:15 +0530

    updating files

--- lib/pages/agro_weather.dart
+++ lib/pages/agro_weather.dart
@@ -1,30 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 
-class ArgoWeather extends StatelessWidget {
-  const ArgoWeather({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Info Page'),
-      ),
-      body: ListView(
-        children: [
-          GFImageOverlay(
-            height: 400,
-            width: 300,
-            child: Center(
-              child: Text('Weather', style:TextStyle(color: GFColors.LIGHT, fontSize: 50 , fontWeight: FontWeight.bold)),
-            ),
-            image: AssetImage('images/weather-banner.jpg'),
-            colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3),
-            BlendMode.darken),
-          ),
-        ],
-      )
-    );
-  }
-
-}
+class AgroWeather extends StatelessWidget {
+  const AgroWeather({Key? key}) : super(key: key);
