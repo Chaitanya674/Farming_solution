b7076729ed4c89c7a14dfc04b95cc7b629e5bab2
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-21 16:58:41 +0530

    updating files

--- lib/pages/agro_weather.dart
+++ lib/pages/agro_weather.dart
@@ -3,3 +3,10 @@ import 'package:getwidget/getwidget.dart';
 
 class AgroWeather extends StatelessWidget {
   const AgroWeather({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agricultural Weather'),
+      ),
