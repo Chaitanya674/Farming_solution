7ef292cef1a54065d4a9e508ad33c550fb817ace
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-27 17:35:54 +0530

    updating files

--- lib/pages/seedsandsowing.dart
+++ lib/pages/seedsandsowing.dart
@@ -5,3 +5,18 @@ import 'package:flutter_tts/flutter_tts.dart';
 
 class SeedsSowing extends StatelessWidget {
   const SeedsSowing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Info Page'),
+      ),
+      body: ListView(
+        children: [
+          GFImageOverlay(
+            height: 400,
+            width: 300,
+            child: Center(
+              child: Text(
+                'Seed Sowing',
