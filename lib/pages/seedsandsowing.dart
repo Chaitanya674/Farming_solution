cdbafbf37ffc9c2d17f5ddc78e29f6ccaefeb5f0
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-27 17:37:07 +0530

    updating files

--- lib/pages/seedsandsowing.dart
+++ lib/pages/seedsandsowing.dart
@@ -54,3 +54,30 @@ class SeedsSowing extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: Text(
               "Sowing (or seed sowing) is defined as the process of placing the seed in soil to germinate and grow into a plant. In comparison, planting is the putting of plant propagules in soil for growing plants. Propagules can be seedlings, roots, tubers, leaves, or cuttings. Transplanting is a term used for planting seedlings grown in nurseries into different fields, pots, or plots for different purposes.",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Methods of Sowing",
+                  style: TextStyle(
+                    color: Colors.grey.shade700,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "Seeds may be sown directly or transplanted. For transplanting, the seeds are sown in a nursery, and the nursery is later transplanted to the field. The methods of sowing are enlisted and detailed as follows;\n\na) Broadcasting\nb) Dibbling\nc) Drilling\nd) Sowing behind the country plough\ne) Planting\nf) Transplanting",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
