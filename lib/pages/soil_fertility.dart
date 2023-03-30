be2bab0756d04c99a6b4360074553fac59da0ee4
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-03-30 17:42:53 +0530

    updating files

---
+++ lib/pages/soil_fertility.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+class SoilAndFertility extends StatelessWidget {
+  const SoilAndFertility({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agriculture Nutrient Management and Fertilizer'),
+      ),
