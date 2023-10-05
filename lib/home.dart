36f105c749b9f913cfbf72aee76863d1ddf63cdb
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-10-05 20:12:20 +0530

    Updating the app

--- lib/home.dart
+++ lib/home.dart
@@ -3,8 +3,25 @@ import 'image_slider.dart';
 import 'info_card.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-class HomePage  extends StatelessWidget {
-  const HomePage ({ Key? key }) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final FlutterTts flutterTts = FlutterTts();
+
+  Widget buildRightRow({
+    required String imageSrc,
+    required String content,
+    required String title,
+    required String location,
+  }) {
+    return RightRow(
+      image_src: imageSrc,
+      content: content,
+      title: title,
+      location: location,
+      flutterTts: flutterTts,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,55 +30,81 @@ class HomePage  extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.green.shade700,
         centerTitle: true,
-        title: Text("Welcome to Kisan Vikas" , textAlign: TextAlign.center),
+        title: Text("Welcome to Kisan Vikas", textAlign: TextAlign.center),
       ),
       body: ListView(
         children: [
-          SizedBox(height: 20,),
+          SizedBox(height: 20),
           ImageSlider(),
-          
-          SizedBox(height: 20,),
-          RightRow(image_src: 'images/advicer.jpg',
-          content: 'Advisory related to finances and farming are provider here. If you are suffering with financial conditions and farming click the button, Fill the asked details and we will give the best possible solutions.',
-          title: "Advisory Service", location: 'Advice', flutterTts: FlutterTts(),),
-          RightRow(image_src: 'images/kisan-scheme.png',
-          content: 'All the information related to goverment schemes are here. If you find the scheme beneficial for you click the botton below : ',
-          title: "Goverment Schemes" , location: 'info_page_2',flutterTts: FlutterTts(),),
-          RightRow(image_src: 'images/Farmer-Tractor.jpg',
-          content: 'All the information related to argicultural machineries are here. If want to know more about them and their use please click the button below:',
-          title: "Farm Machinery", location: 'agri_mach',flutterTts: FlutterTts(),),
-          RightRow(image_src: 'images/seeds.jpg',
-          content: 'All the information related to seeds and plant having any disease is detected and treated here :',
-          title: "Disease Detection",location: 'Detect',flutterTts: FlutterTts(),),
-          RightRow(image_src: 'images/soil.jpg',
-          content: 'All the information related to soil are here. If want to know more about please click the button below:',
-          title: "Soil and fertility", location: 'soilandfertility',flutterTts: FlutterTts(),),
-          RightRow(image_src: 'images/marketing.jpg',
-          content: 'All the information related to marketing of agricultural products and the best ways to get more profits are here. If want to know more about please click the button below:',
-          title: "Agricultural Marketing", location: 'agro_market',flutterTts: FlutterTts(),),
+          SizedBox(height: 20),
+          buildRightRow(
+            imageSrc: 'images/advicer.jpg',
+            content:
+                'Advisory related to finances and farming are provided here. If you are suffering from financial conditions and farming issues, click the button, fill in the asked details, and we will provide the best possible solutions.',
+            title: "Advisory Service",
+            location: 'Advice',
+          ),
+          buildRightRow(
+            imageSrc: 'images/kisan-scheme.png',
+            content:
+                'All the information related to government schemes is here. If you find a scheme beneficial for you, click the button below:',
+            title: "Government Schemes",
+            location: 'info_page_2',
+          ),
+          buildRightRow(
+            imageSrc: 'images/Farmer-Tractor.jpg',
+            content:
+                'All the information related to agricultural machineries is here. If you want to know more about them and their use, please click the button below:',
+            title: "Farm Machinery",
+            location: 'agri_mach',
+          ),
+          buildRightRow(
+            imageSrc: 'images/seeds.jpg',
+            content:
+                'All the information related to seeds and plants with diseases detected and treated here:',
+            title: "Disease Detection",
+            location: 'Detect',
+          ),
+          buildRightRow(
+            imageSrc: 'images/soil.jpg',
+            content:
+                'All the information related to soil is here. If you want to know more, please click the button below:',
+            title: "Soil and Fertility",
+            location: 'soilandfertility',
+          ),
+          buildRightRow(
+            imageSrc: 'images/marketing.jpg',
+            content:
+                'All the information related to marketing of agricultural products and the best ways to get more profits is here. If you want to know more, please click the button below:',
+            title: "Agricultural Marketing",
+            location: 'agro_market',
+          ),
           Padding(
             padding: EdgeInsets.all(10),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(image:new AssetImage('images/Sabka-sath-sabka-vikas.jpg'),fit: BoxFit.fill,)]),
+                Image(
+                  image: AssetImage('images/Sabka-sath-sabka-vikas.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ],
+            ),
           ),
         ],
       ),
-    persistentFooterButtons: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 25,
-          )
-        ],
-      )
-    ],
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 25,
+            )
+          ],
+        )
+      ],
     );
-
-
   }
 }
